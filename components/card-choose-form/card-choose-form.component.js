4f27cc51a8e08e3733556dfa973a2462def14c39
Wastard <Ilya.pakylov@gmail.com>
2023-09-29 17:26:35 +0300

    added card-selected & circle-selected css classes

--- components/card-choose-form/card-choose-form.component.js
+++ components/card-choose-form/card-choose-form.component.js
@@ -8,3 +8,20 @@ export const cardChooseForm = async () => {
     const data = await (await fetch(moduleUrl)).text()
     document.querySelector('#card-choose-form-container').innerHTML = data
 }
+
+export const handleCardContainerClick = () => {
+    const items = document.querySelectorAll('.card-choose-form-card-container')
+    let lastSelected
+
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            if (lastSelected) {
+                lastSelected.classList.remove(
+                    'card-choose-form-card-container-selected'
+                )
+            }
+            this.classList.add('card-choose-form-card-container-selected')
+            lastSelected = this
+        })
+    })
+}
