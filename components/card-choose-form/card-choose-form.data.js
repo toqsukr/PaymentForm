a235f55b7dd5b8a536bf53ca24b94b30b779ab0c
Wastard <Ilya.pakylov@gmail.com>
2023-10-05 22:52:15 +0300

    dedicated card component

---
+++ components/card-choose-form/card-choose-form.data.js
@@ -0,0 +1,20 @@
+export const cards = [
+    {
+        imageURL: '/images/visa.png',
+        imageAlt: '',
+        cardNumber: '5824 2044 7100 8908',
+        cardExpires: '09/27',
+    },
+    {
+        imageURL: '/images/mastercard.png',
+        imageAlt: '',
+        cardNumber: '2202 2002 4722 7777',
+        cardExpires: '01/24',
+    },
+    {
+        imageURL: '/images/paypal.png',
+        imageAlt: '',
+        cardNumber: '6936 5936 5482 6498',
+        cardExpires: '12/23',
+    },
+]
