1503b4f3846a37794e248b0e5fa3e887c5a51c6a
Wastard <Ilya.pakylov@gmail.com>
2023-10-05 20:15:15 +0300

    added switch card logic

--- components/card-choose-form/card-choose-form.component.js
+++ components/card-choose-form/card-choose-form.component.js
@@ -7,21 +7,19 @@ export const cardChooseForm = async () => {
     )
     const data = await (await fetch(moduleUrl)).text()
     document.querySelector('#card-choose-form-container').innerHTML = data
+    handleCardContainerClick()
 }
 
 export const handleCardContainerClick = () => {
     const items = document.querySelectorAll('.card-choose-form-card-container')
-    let lastSelected
-
+    let lastElement
     items.forEach(item => {
         item.addEventListener('click', () => {
-            if (lastSelected) {
-                lastSelected.classList.remove(
-                    'card-choose-form-card-container-selected'
-                )
+            if (!item.classList.contains('.card-selected')) {
+                if (lastElement) lastElement.classList.remove('card-selected')
+                item.classList.add('card-selected')
+                lastElement = item
             }
-            this.classList.add('card-choose-form-card-container-selected')
-            lastSelected = this
         })
     })
 }
