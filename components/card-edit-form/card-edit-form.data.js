ef98c437ff315c4393374c940ec1dbe95b8de18b
Wastard <Ilya.pakylov@gmail.com>
2023-10-08 13:21:40 +0300

    added CARD VALIDATION

--- components/card-edit-form/card-edit-form.data.js
+++ components/card-edit-form/card-edit-form.data.js
@@ -3,20 +3,50 @@ export const inputs = [
         name: 'name',
         label: 'Name',
         type: 'text',
+        placeholder: 'Name Surname',
+        maxlength: 50,
+        oninput: validateCardOwner,
     },
     {
         name: 'expiration',
         label: 'Expiration',
         type: 'text',
+        placeholder: 'MM/YY',
+        maxlength: 5,
+        oninput: validateExpirationDate,
     },
     {
         name: 'cardNumber',
         label: 'Card Number',
         type: 'text',
+        placeholder: 'XXXX XXXX XXXX XXXX',
+        maxlength: 19,
+        oninput: validateCardNumber,
     },
     {
         name: 'code',
         label: 'CVV',
         type: 'password',
+        placeholder: 'XXX',
+        maxlength: 3,
+        oninput: validateCVV,
     },
 ]
+
+function validateCardOwner() {
+    this.value = this.value.replace(/[^a-zA-Z\s]/g, '')
+}
+
+function validateCardNumber() {
+    this.value = this.value.replace(/\D/g, '').replace(/(\d{4})(?=\d)/g, '$1 ')
+}
+
+function validateExpirationDate() {
+    this.value = this.value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d{0,2})/, '$1/$2')
+}
+
+function validateCVV() {
+    this.value = this.value.replace(/\D/g, '')
+}
--- components/card-edit-form/input/input.component.js
+++ components/card-edit-form/input/input.component.js
@@ -1,12 +1,26 @@
 import css from './input.module.css'
 
-export const inputComponent = ({ label, type, name }) => {
+export const inputComponent = ({
+    label,
+    type,
+    name,
+    placeholder,
+    oninput,
+    maxlength,
+}) => {
     const inputContent = `
         <span>${label}</span>
-        <input name=${name} type=${type} />
   `
+
+    const input = document.createElement('input')
+    input.name = name
+    input.type = type
+    input.placeholder = placeholder
+    input.maxLength = maxlength
+    input.oninput = oninput
     const inputElement = document.createElement('div')
     inputElement.className = css.editorCell
     inputElement.innerHTML = inputContent
+    inputElement.appendChild(input)
     return inputElement
 }
