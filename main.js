2a0f561f7b9c3494dbd01805cd8dda54760595b9
Wastard <Ilya.pakylov@gmail.com>
2023-10-12 14:23:21 +0300

    added routes; added redirect

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 import { cardChooseFormComponent } from './components/card-choose-form/card-choose-form.component'
 import './style.css'
+import { redirect } from './utils/functions'
+import { Routes } from './utils/routes'
+
+redirect()
 
 const cardChooseForm = await cardChooseFormComponent()
 
-document.getElementById('app-inner-container').appendChild(cardChooseForm)
+const urlContent = {
+  [Routes.HOME]: '',
+  [Routes.PAYMENT_METHODS]: cardChooseForm,
+}
+
+const pathname = window.location.pathname
+const content = urlContent[pathname]
+
+document.getElementById('app-inner-container').appendChild(content)
--- utils/functions.js
+++ utils/functions.js
@@ -1,3 +1,5 @@
+import { Routes } from './routes'
+
 export const saveToStorage = (data, key) => {
   const dataString = JSON.stringify(data)
   localStorage.setItem(key, dataString)
@@ -7,4 +9,10 @@ export const getFromStorage = key => {
   const dataString = localStorage.getItem(key)
   const data = JSON.parse(dataString)
   return data
+}
+
+export const redirect = () => {
+  if (!Object.values(Routes).includes(window.location.pathname)) {
+    window.location.pathname = Routes.HOME
+  }
 }
